f11426ab602ba130e17d70908b4f4b5a40352e4b
Jeremy Duval <113064972+JeremyDVL@users.noreply.github.com>
2023-07-04 09:14:48 -0400

    update

--- config/server.js
+++ config/server.js
@@ -1,10 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  url: env("https://strapi-food-ordering-app.herokuapp.com/"),
+  proxy: true,
   app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
   },
 });
